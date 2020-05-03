e5e17edebd21806ccdad016573f8096cf3641a86
willisfusu <liweicfd@hotmail.com>
2020-05-04 00:03:34 +0100

    first:rocket:

---
+++ logo-rui.js
@@ -0,0 +1,138 @@
+function init() {
+  logo("#logo");
+}
+function supportsSVG() {
+  return !!document.createElementNS && !!document
+    .createElementNS('http://www.w3.org/2000/svg', "svg").createSVGRect;
+}
+
+function logo(el) {
+  if (!supportsSVG()) { return; }
+
+  var data = [
+      [-9,2,0],[-9,0,0],[-9,-2,0],[-9,-4,0],[-9,-6,0],
+      [-7,2,0],[-7,-2,0],[-7,-4,0],
+      [-5,2,0],[-5,0,0],[-5,-4,0],[-5,-6,0],
+
+      [-2,0,0],[-2,-2,0],[-2,-4,0],[-2,-6,0],
+      [2,0,0],[2,-2,0],[2,-4,0],[0,-6,0],[4,-6,2],
+
+      [7,4,2],[7,0,0],[7,-2,0],[7,-4,0],[7,-6,0]
+  ];
+  var letter = ["b", "u", "g"],
+      space  = 25,
+      power  = -400,
+      cell   = 9,
+      pad    = 1,
+      // w      = cell*7 + space*2 + 100,
+      // h      = cell*5 + space*2,
+      w      = cell*7+space*1.5,
+      h      = cell*5+space*0.5,
+      // tx     = Math.round(space + 3.5*cell),
+      // ty     = Math.round(space + 2.0*cell),
+      tx     = Math.round(2.5*cell+space),
+      ty     = Math.round(2*cell),
+      N      = data.length,
+      nodes  = data.slice();
+  // fade out pre-existing background image
+  // d3.select(el).select("img")
+  //   .style("opacity", 0.1);
+   // initialize logo
+  var svg = d3.select(el)
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
+
+  var g = svg.append("g")
+    .attr("transform", "translate("+tx+","+ty+")");
+
+  var lr = g.selectAll("rect")
+    .data(nodes)
+   .enter().append("rect")
+    .attr("class", function(d) { return "logo-"+letter[d[2]]; })
+    // .attr("x", function(d) { return d.x = Math.round(d[0]/2 * cell); })
+    // .attr("y", function(d) { return d.y = Math.round(d[1]/2 * -cell); })
+    .attr("x", function(d) { return d.x = Math.round(d[0]/2*cell); })
+    .attr("y", function(d) { return d.y = Math.round(d[1]/2*-cell); })
+    .attr("width", (cell - pad))
+    .attr("height", (cell - pad));
+
+//   // add force layout interaction
+  function make(o) { return {x:o.x, y:o.y, fixed:true, radius:0}; }
+    nodes.push.apply(nodes, nodes.map(make));
+  nodes.unshift(make({x:10000, y:10000}));
+  for(var i=0, len=nodes.length; i<len; i++){
+      console.log(nodes[i])
+  }
+  var links = d3.range(N).map(function(i) {
+      return {source:i+1, target:i+N+1};
+  });
+  // var svg_edges = g.selectAll("line")
+  //     .data(links)
+  //     .enter()
+  //     .append("line")
+  //     .style("stroke","#ccc")
+  //     .style("stroke-width",1);
+//
+  var force = d3.layout.force()
+    .gravity(0)
+    .charge(function(d, i) { return i ? 0 : power; })
+    .linkDistance(0.2)
+    .nodes(nodes)
+    .links(links)
+    .start()
+    .on("tick", function() {
+      // for (var i=0, q=d3.geom.quadtree(nodes); i<N; ++i) {
+      //   q.visit(logo_collide(nodes[i+1]));
+      // }
+      lr.attr("x", function(d) { return Math.round(d.x); })
+        .attr("y", function(d) { return Math.round(d.y); });
+    });
+//
+
+//
+  function move() {
+     var p1 = d3.mouse(this);
+    nodes[0].px = p1[0] - tx;
+    nodes[0].py = p1[1] - ty;
+    force.resume();
+  }
+  function off() {
+    nodes[0].px = nodes[0].py = 10000;
+    force.resume();
+  }
+//
+  svg
+    .on("mousemove", move)
+    .on("touchmove", move)
+    .on("mouseout", off)
+    .on("touchend", off);
+
+// function logo_collide(node) {
+//         var r = node.radius + 16,
+//         nx1 = node.x - r,
+//         nx2 = node.x + r,
+//         ny1 = node.y - r,
+//         ny2 = node.y + r;
+//     return function (quad, x1, y1, x2, y2) {
+//         if (quad.point && (quad.point !== node)) {
+//             var x = node.x - quad.point.x,
+//                 y = node.y - quad.point.y,
+//                 l = Math.sqrt(x * x + y * y),
+//                 r = node.radius + quad.point.radius;
+//             if (l < r) {
+//                 l = (l - r) / l * .5;
+//                 node.x -= x *= l;
+//                 node.y -= y *= l;
+//                 quad.point.x += x;
+//                 quad.point.y += y;
+//             }
+//         }
+//         return x1 > nx2
+//             || x2 < nx1
+//             || y1 > ny2
+//             || y2 < ny1;
+//     };
+// }
+}
+
---
+++ main.js
@@ -0,0 +1,81 @@
+init();
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+            anchors: ['page1', 'page2', 'page3', 'page4'],
+            navigation: true,
+            navigationPosition: 'right',
+            easing: 'easeInOutCubic',
+            easingcss3: 'ease',
+            autoScrolling:false
+        }
+    );
+    var myCS= { thumbnail : {borderColor: '#F8F8FF' } };
+    $("#nanogallery2").nanogallery2({
+        thumbnailHeight:  200,
+        thumbnailWidth: "auto",
+        thumbnailLabel:{display:"false"},
+        galleryTheme: myCS,
+        thumbnailDisplayTransition:'slideRight',
+        thumbnailDisplayTransitionDuration:500,
+        thumbnailDisplayInterval:30,
+        galleryDisplayMode:"rows",
+        galleryMaxRows:4,
+        items: [
+            { src: './images/1.jpg', srct: './images/thumb/1.jpg', title: 'Title Image 1' },
+            { src: './images/2.jpg', srct: './images/thumb/2.jpg', title: 'Title Image 1' },
+            { src: './images/5.jpg', srct: './images/thumb/5.jpg', title: 'Title Image 1' },
+            { src: './images/7.jpg', srct: './images/thumb/7.jpg', title: 'Title Image 1' },
+            { src: './images/6.jpg', srct: './images/thumb/6.jpg', title: 'Title Image 1' },
+            { src: './images/8.jpg', srct: './images/thumb/8.jpg', title: 'Title Image 1' },
+            { src: './images/10.jpg', srct: './images/thumb/10.jpg', title: 'Title Image 1' },
+            { src: './images/11.jpg', srct: './images/thumb/11.jpg', title: 'Title Image 1' },
+            { src: './images/13.jpg', srct: './images/thumb/13.jpg', title: 'Title Image 1' },
+            { src: './images/14.jpg', srct: './images/thumb/14.jpg', title: 'Title Image 1' },
+            { src: './images/23.jpg', srct: './images/thumb/23.jpg', title: 'Title Image 1' },
+            { src: './images/24.jpg', srct: './images/thumb/24.jpg', title: 'Title Image 1' },
+            { src: './images/25.jpg', srct: './images/thumb/25.jpg', title: 'Title Image 1' },
+            { src: './images/28.jpg', srct: './images/thumb/28.jpg', title: 'Title Image 1' },
+            { src: './images/29.jpg', srct: './images/thumb/29.jpg', title: 'Title Image 1' },
+            { src: './images/9.jpg', srct: './images/thumb/9.jpg', title: 'Title Image 1' },
+            { src: './images/12.jpg', srct: './images/thumb/12.jpg', title: 'Title Image 1' },
+            { src: './images/15.jpg', srct: './images/thumb/15.jpg', title: 'Title Image 1' },
+            { src: './images/17.jpg', srct: './images/thumb/17.jpg', title: 'Title Image 1' },
+            { src: './images/20.jpg', srct: './images/thumb/20.jpg', title: 'Title Image 1' },
+            { src: './images/22.jpg', srct: './images/thumb/22.jpg', title: 'Title Image 1' },
+            { src: './images/27.jpg', srct: './images/thumb/27.jpg', title: 'Title Image 1' },
+        ]
+    });
+
+});
+
+var offsetX = $("#loveHeart").width() / 2;
+var offsetY = $("#loveHeart").height() / 2 - 55;
+var together = new Date();
+together.setFullYear(2019, 8, 3);
+together.setHours(12);
+together.setMinutes(0);
+together.setSeconds(0);
+together.setMilliseconds(0);
+
+if (!document.createElement('canvas').getContext) {
+    var msg = document.createElement("div");
+    msg.id = "errorMsg";
+    msg.innerHTML = "Your browser doesn't support HTML5!<br/>Recommend use Chrome 14+/IE 9+/Firefox 7+/Safari 4+";
+    document.body.appendChild(msg);
+    $("#code").css("display", "none")
+    $("#copyright").css("position", "absolute");
+    $("#copyright").css("bottom", "10px");
+    document.execCommand("stop");
+} else {
+    setTimeout(function () {
+        startHeartAnimation();
+    }, 5000);
+
+    timeElapse(together);
+    setInterval(function () {
+        timeElapse(together);
+    }, 500);
+
+    adjustCodePosition();
+    $("#code").typewriter();
+}
